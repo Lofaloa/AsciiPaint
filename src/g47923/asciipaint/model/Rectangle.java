ab73a24e8a6cff23480211bcc4770d5b1ee18d59
g47923 <g47923@L405P14.pdg.esi.heb.site>
2018-09-28 17:57:24 +0200

    travaail

---
+++ src/g47923/asciipaint/model/Rectangle.java
@@ -0,0 +1,76 @@
+package g47923.asciipaint.model;
+
+/**
+ * Represents a rectangle.
+ *
+ * @author g47923
+ */
+public class Rectangle extends ColoredShape {
+
+    private Point upperLeft;
+    private double width;
+    private double height;
+
+    /**
+     * Initializes this Rectangle the the given position, width, height and
+     * color.
+     *
+     * @param upperLeft is the upper left point of this rectangle.
+     * @param width is the width of this rectangle.
+     * @param height is the height of this rectangle.
+     * @param color is the color of this rectangle.
+     */
+    public Rectangle(Point upperLeft, double width, double height, char color) {
+        super(color);
+        if (height < 0) {
+            throw new IllegalArgumentException(height + " is not valid, the height"
+                    + " should be positive.");
+        }
+        if (width < 0) {
+            throw new IllegalArgumentException(width + " is not valid, the width"
+                    + " should be positive.");
+        }
+        this.upperLeft = new Point(upperLeft.getX(), upperLeft.getY());
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * Gets the upperLeft point of this rectangle.
+     *
+     * @return the upperLeft point of this rectangle.
+     */
+    Point getUpperLeft() {
+        return new Point(upperLeft.getX(), upperLeft.getY());
+    }
+
+    /**
+     * Gets the width of this rectangle.
+     *
+     * @return the width of this rectangle.
+     */
+    double getWidth() {
+        return width;
+    }
+
+    /**
+     * Gets the height of this rectangle.
+     *
+     * @return the height of this rectangle.
+     */
+    double getHeight() {
+        return height;
+    }
+
+    @Override
+    public void move(double dx, double dy) {
+        this.upperLeft.move(dx, dy);
+    }
+
+    @Override
+    public boolean isInside(Point p) {
+        return p.getX() <= upperLeft.getX() + width
+                && p.getY() <= upperLeft.getY() + height;
+    }
+
+}
---
+++ test/g47923/asciipaint/model/RectangleTest.java
@@ -0,0 +1,65 @@
+package g47923.asciipaint.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for the Rectangle class.
+ *
+ * @author g47923
+ */
+public class RectangleTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void rectangleInitializationNegativeWidth() {
+        Point p = new Point(2, 4);
+        Rectangle r = new Rectangle(p, -4, 16, 'a');
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void rectangleInitializationNegativeHeight() {
+        Point p = new Point(2, 4);
+        Rectangle r = new Rectangle(p, 4, -16, 'a');
+    }
+
+    @Test
+    public void rectangleInitialization() {
+        Point p = new Point(2, 4);
+        Rectangle r = new Rectangle(p, 4, 16, 'a');
+        assertTrue(r.getUpperLeft().getX() == 2 && r.getUpperLeft().getY() == 4
+                && r.getWidth() == 4 && r.getHeight() == 16 && r.getColor() == 'a');
+    }
+    
+    @Test
+    public void moveRectangle() {
+        Point p = new Point(2, 4);
+        Rectangle r = new Rectangle(p, 4, 16, 'a');
+        p.move(10, 65);
+        r.move(2, 4);
+        assertTrue(r.getUpperLeft().getX() == 4 && r.getUpperLeft().getY() == 8);
+    }
+    
+    @Test
+    public void isInsideInternalPoint() {
+        Point p = new Point(2, 4);
+        Point p1 = new Point(6, 21);
+        Rectangle r = new Rectangle(p, 4, 16, 'a');
+        assertTrue(r.isInside(p1));    
+    }
+
+    @Test
+    public void isNotInsideExternalPoint() {
+        Point p = new Point(2, 4);
+        Point p1 = new Point(8, 21);
+        Rectangle r = new Rectangle(p, 4, 16, 'a');
+        assertFalse(r.isInside(p1));
+    }
+    
+    @Test
+    public void isInsideUpperLeftPoint() {
+        Point p = new Point(2, 4);
+        Rectangle r = new Rectangle(p, 4, 16, 'a');
+        assertTrue(r.isInside(p));
+    }
+    
+}
