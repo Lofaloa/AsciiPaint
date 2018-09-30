6485e6e080f6ee9e57944540bcba51ab2f3429bb
Logan Farci <farci.logan@gmail.com>
2018-09-30 18:31:39 +0200

    AsciiPaint class est implémentée et testée.

--- src/g47923/asciipaint/model/AsciiPaint.java
+++ src/g47923/asciipaint/model/AsciiPaint.java
@@ -1,21 +1,99 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package g47923.asciipaint.model;
 
 /**
+ * Represents the game.
  *
  * @author g47923
  */
 public class AsciiPaint {
 
+    private final Drawing drawing;
+
     /**
-     * @param args the command line arguments
+     * Initializes this game with a empty blank drawing of 50 x 50;
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public AsciiPaint() {
+        this.drawing = new Drawing();
     }
-    
+
+    /**
+     * Initializes this game with a given blank drawing.
+     *
+     * @param drawing is the drawing of this game.
+     */
+    public AsciiPaint(Drawing drawing) {
+        if (!drawing.getShapes().isEmpty()) {
+            throw new IllegalArgumentException("The drawing should be empty.");
+        }
+        this.drawing = new Drawing(drawing.getHeight(), drawing.getWidth());
+    }
+
+    /**
+     * Gets this game drawing.
+     *
+     * @return this game drawing.
+     */
+    Drawing getDrawing() {
+        return drawing;
+    }
+
+    /**
+     * Adds a new circle to this game drawing.
+     *
+     * @param x is the x position of the circle center.
+     * @param y is the y position of the circle center.
+     * @param radius is the size of the circle radius.
+     * @param color is the color if the circle.
+     */
+    public void newCircle(int x, int y, double radius, char color) {
+        drawing.addShape(new Circle(new Point(x, y), radius, color));
+    }
+
+    /**
+     * Adds a new rectangle to this game drawing.
+     *
+     * @param x is the x position of the rectangle upper left point.
+     * @param y is the y position of the rectangle upper left point.
+     * @param width is the width of the rectangle.
+     * @param height is the height of the rectangle.
+     * @param color is the color if the rectangle.
+     */
+    public void newRectangle(int x, int y, double width, double height, char color) {
+        drawing.addShape(new Rectangle(new Point(x, y), width, height, color));
+    }
+
+    /**
+     * Adds a new square to this game drawing.
+     *
+     * @param x is the x position of the square upper left point.
+     * @param y is the y position of the square left point.
+     * @param side is the side of the square.
+     * @param color is the color if the square.
+     */
+    public void newSquare(int x, int y, double side, char color) {
+        drawing.addShape(new Square(new Point(x, y), side, color));
+    }
+
+    /**
+     * Represents this game drawing.
+     *
+     * @return a representation of this game drawing.
+     */
+    public String asAscii() {
+        String str = "";
+        for (int i = 0; i < drawing.getHeight(); i++) {
+            for (int j = 0; j < drawing.getWidth(); j++) {
+                Point currentPoint = new Point(i, j);
+                Shape s = drawing.getShapeAt(currentPoint);
+                if (s != null && s.isInside(currentPoint)) {
+                    str += s.getColor();
+                } else {
+                    str += " ";
+                }
+            }
+            str += '\n';
+        }
+        return str;
+    }
+
 }
---
+++ test/g47923/asciipaint/model/AsciiPaintTest.java
@@ -0,0 +1,88 @@
+package g47923.asciipaint.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for the AsciiPaint class.
+ *
+ * @author Logan
+ */
+public class AsciiPaintTest {
+
+    @Test
+    public void initilizationWithoutArguments() {
+        AsciiPaint ap = new AsciiPaint();
+        assertTrue(ap.getDrawing().getHeight() == 50
+                && ap.getDrawing().getWidth() == 50
+                && ap.getDrawing().getShapes().isEmpty());
+    }
+
+    @Test
+    public void initilizationWithEmptyDrawing() {
+        Drawing d = new Drawing(100, 30);
+        AsciiPaint ap = new AsciiPaint(d);
+        assertTrue(ap.getDrawing().getHeight() == 100
+                && ap.getDrawing().getWidth() == 30
+                && ap.getDrawing().getShapes().isEmpty()
+                && ap.getDrawing() != d);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void initilizationWithNotEmptyDrawing() {
+        Drawing d = new Drawing(100, 30);
+        d.addShape(new Square(new Point(), 4, 'a'));
+        AsciiPaint ap = new AsciiPaint(d);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void newCircleNegativeRadius() {
+        Drawing d = new Drawing(100, 30);
+        AsciiPaint ap = new AsciiPaint(d);
+        ap.newCircle(0, 0, -4, 'a');
+    }
+
+    public void newCircleAddedToDrawing() {
+        Drawing d = new Drawing(100, 30);
+        AsciiPaint ap = new AsciiPaint(d);
+        ap.newCircle(0, 0, 4, 'a');
+        assertTrue(ap.getDrawing().getShapes().get(0) instanceof Circle);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void newRectangleNegativeHeight() {
+        Drawing d = new Drawing(100, 30);
+        AsciiPaint ap = new AsciiPaint(d);
+        ap.newRectangle(0, 0, 5, -4, 'a');
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void newRectangleNegativeWidth() {
+        Drawing d = new Drawing(100, 30);
+        AsciiPaint ap = new AsciiPaint(d);
+        ap.newRectangle(0, 0, -5, 4, 'a');
+    }
+
+    public void newRectangleAddedToDrawing() {
+        Drawing d = new Drawing(100, 30);
+        AsciiPaint ap = new AsciiPaint(d);
+        ap.newRectangle(0, 0, 3, 5, 'a');
+        assertTrue(ap.getDrawing().getShapes().get(0) instanceof Rectangle);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void newSquareNegativeSide() {
+        Drawing d = new Drawing(100, 30);
+        AsciiPaint ap = new AsciiPaint(d);
+        ap.newSquare(0, 0, -1, 'a');
+    }
+
+    @Test
+    public void newSquareAddedToDrawing() {
+        Drawing d = new Drawing(100, 30);
+        AsciiPaint ap = new AsciiPaint(d);
+        ap.newSquare(0, 0, 2, 'a');
+        assertTrue(ap.getDrawing().getShapes().get(0) instanceof Square);
+    }
+
+}
