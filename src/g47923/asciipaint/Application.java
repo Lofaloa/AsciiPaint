c53b81e4cb21b595ecf88f3d13a9a3035339a231
Lofaloa <farci.logan@gmail.com>
2018-10-05 11:34:47 +0200

    The user can enter arguments in command.

--- src/g47923/asciipaint/Application.java
+++ src/g47923/asciipaint/Application.java
@@ -12,15 +12,44 @@ import g47923.asciipaint.model.Drawing;
 public class Application {
 
     public static void main(String[] args) {
-        int width = 50;
-        int height = 50;
-        try {
-            width = Integer.parseInt(args[0]);
-            height = Integer.parseInt(args[1]);
-        } catch (NumberFormatException ex) {
-            System.err.println("usage: invalid arguments, it should be the width"
-                    + " and height od the drawing.");
+        if (args.length == 1 || args.length > 2) {
+            System.out.println("usage: none or two arguments are required.");
+        } else if (args.length == 0) {
+            startDefault();
+        } else {
+            int width = 50;
+            int height = 50;
+            try {
+                width = Integer.parseInt(args[0]);
+                height = Integer.parseInt(args[1]);
+            } catch (NumberFormatException ex) {
+                System.err.println("usage: invalid arguments, it should be the width"
+                        + " and height od the drawing.");
+            }
+            try {
+                startCustom(width, height);
+            } catch (IllegalArgumentException ex) {
+                System.err.println(ex.getMessage());
+            }
+
         }
+
+    }
+
+    /**
+     * Starts this program with default width and height for the drawing. By
+     * default, the drawing is 50 x 50.
+     */
+    public static void startDefault() {
+        AsciiPaint asciiPaint = new AsciiPaint(new Drawing());
+        Controller c = new Controller(asciiPaint);
+        c.start();
+    }
+
+    /**
+     * Starts this program with custom width and height for the drawing.
+     */
+    public static void startCustom(int width, int height) {
         AsciiPaint asciiPaint = new AsciiPaint(new Drawing(width, height));
         Controller c = new Controller(asciiPaint);
         c.start();
